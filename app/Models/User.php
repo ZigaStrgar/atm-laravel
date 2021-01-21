5b9668b3b40d5ba2ab6bd28b1913a57f4dfe20e7
ZigaStrgar <ziga.strgar@gmail.com>
2021-01-21 20:06:08 +0100

    Models, migrations, countries package

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,33 +10,20 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function getCountryDetailsAttribute()
+    {
+        return Countries::where('cca2', $this->country);
+    }
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function canWithdraw($amount): bool
+    {
+        return $this->amount >= $amount;
+    }
 }
---
+++ database/migrations/2021_01_19_175734_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique()->index();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('gender', ['female', 'male', 'undefined'])->default('undefined');
+            $table->string('country')->nullable()->index();
+            $table->float('bonus', 3);
+            $table->float('balance', 15, 5)->default(0);
+            $table->float('bonus_balance', 15, 5)->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
